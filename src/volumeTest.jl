8ae26dff31772796baae79d2b8a79496df06a4de
Simon Danisch <sdanisch@gmail.com>
2014-06-20 19:34:53 +0200

    cleaned up repository

--- src/volumeTest.jl
+++ src/volumeTest.jl
@@ -1,45 +1,19 @@
 using GLWindow, GLUtil, ModernGL, Meshes, Events, React, Images, ImmutableArrays
 
 
-
-function renderObject(renderObject::RenderObject)
-	glEnable(GL_DEPTH_TEST)
-	glEnable(GL_CULL_FACE)
-	glCullFace(GL_BACK)
-	enableTransparency()
-	programID = renderObject.vertexArray.program.id
-	glUseProgram(programID)
-	render(renderObject.uniforms)
-	render(renderObject.vertexArray)
-end
+window 	= createwindow("Mesh Display", 1000, 1000 )
 
 
-
-window = createWindow("Volume Display", 1000, 1000 )
-
-include("plotutil.jl")
-
-#dragging = window.inputs[:mousedragged]
-#println(dragging)
-#lift(println,Nothing, dragging)
-
-dragging = window.inputs[:mousedragged]
-clicked = window.inputs[:mousepressed]
-zoom = window.inputs[:scroll_y]
-draggedlast = lift(x -> x[1], foldl((a,b) -> (a[2], b), (Vector2(0.0), Vector2(0.0)), dragging))
-dragdiff = lift(-, dragging, draggedlast)
-dragdiffx = lift(x -> float32(x[1]), Float32, dragdiff)
-dragdiffy = lift(x -> float32(x[2]), Float32, dragdiff)
-cam = Cam(window.inputs[:window_size], dragdiffx, dragdiffy, zoom, Vector3(1.5f0, 0f0, 0f0), Input(Vector3(0.0f0)))
+cam = Cam(window.inputs, Vector3(1.5f0, 1.5f0, 1.5f0))
 
 
-println("creating texture")
+include("volume.jl")
+
+
 volume = createvolume("example/")
 gc()
-println("created successfully")
 
-glDisplay(:volume, renderObject, volume)
-
+gldisplay(:glplot, volume)
 
 glClearColor(0,0,0,0)
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,23 @@
 using GLPlot
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+
+sampleMesh = createSampleMesh()
+gldisplay(sampleMesh)
+
+
+
+#=
+sz 		= [256, 256, 256]
+center 	= iceil(sz/2)
+C3 		= Bool[(i-center[1])^2 + (j-center[2])^2 <= (k^2) / 3 for i = 1:sz[1], j = 1:sz[2], k = sz[3]:-1:1]
+cone 	= C3*uint8(255)
+volume 	= createvolume(cone, shader = mipshader)
+
+gldisplay(volume, shader=mipshader)
+gldisplay(volume, shader=volumeshader)
+
+=#
+
+gldisplay(:xyzaxis, GLPlot.axis)
+startplot()
