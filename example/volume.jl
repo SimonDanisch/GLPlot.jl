a571a9f859ddfa22fc3dc0c5907c6833c2aa16cd
Simon Danisch <sdanisch@gmail.com>
2014-07-23 00:34:17 +0200

    some polishing

--- example/volume.jl
+++ example/volume.jl
@@ -43,7 +43,7 @@ algorithm 	= lift( x -> x==GLFW.KEY_I ? 2f0 : 1f0, filter(x-> x==GLFW.KEY_I || x
 isovalue 	= foldl( (v0, v1) -> v1==GLFW.KEY_UP ? (v0 + 0.01f0) : (v1==GLFW.KEY_DOWN ? (v0 - 0.01f0) : v0), 0.5f0, keypressed)
 stepsize 	= foldl( (v0, v1) -> v1==GLFW.KEY_LEFT ? (v0 + 0.001f0) : (v1==GLFW.KEY_RIGHT ? (v0 - 0.001f0) : v0), 0.001f0, keypressed)
 
-#obj = toopengl(volume, algorithm=algorithm, isovalue=isovalue, stepsize=stepsize, color=Vec3(1,0,0))
+obj = toopengl(volume, algorithm=algorithm, isovalue=isovalue, stepsize=stepsize, color=Vec3(1,0,0))
 #obj = toopengl(imread("someexample.nrrd"), algorithm=algorithm, isovalue=isovalue, stepsize=stepsize, color=Vec3(1,0,0))
 
 # I decided not to fake some kind of Render tree for now, as I don't really have more than a list of render objects currently.
