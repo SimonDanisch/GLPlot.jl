dcb704992d1ceccb640682d48e7ccddbadb2239d
Simon Danisch <sdanisch@gmail.com>
2014-08-14 00:19:13 +0200

    added more pictures

--- example/surface.jl
+++ example/surface.jl
@@ -44,7 +44,9 @@ initplotting()
 function zdata(x1, y1, factor)
     x = (x1 - 0.5) * 15
     y = (y1 - 0.5) * 15
-    Vec1((sin(x) + cos(y)) / 10)
+    R = sqrt(x^2 + y^2)
+    Z = sin(R)/R
+    Vec1(Z)
 end
 function zcolor(z)
     a = Vec4(0,1,0,1)
@@ -58,7 +60,8 @@ colordata = map(zcolor , texdata)
 color = lift(x-> Vec4(sin(x), 0,1,1), Vec4, Timing.every(0.1)) # Example on how to use react to change the color over time
 
 #obj = toopengl(texdata) # This is the base case, where the Matrix is simply mapped as a surface, with white color
-obj = toopengl(texdata, primitive=SURFACE(), color=colordata) # Color can be any matrix or a Vec3
+color = Texture(Pkg.dir()*"/GLPlot/docs/julia.png")
+obj = toopengl(texdata, primitive=CIRCLE(), color=color) # Color can be any matrix or a Vec3
 
 #####################################################################################################################
 # This is basically what the SURFACE() function returns.
