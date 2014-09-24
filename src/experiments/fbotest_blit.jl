3464e0b846a2120ef7f53c623460641bd0b4f3b5
Simon Danisch <sdanisch@gmail.com>
2014-09-24 14:09:20 +0200

    advancing on interaction

--- src/experiments/fbotest_blit.jl
+++ src/experiments/fbotest_blit.jl
@@ -1,7 +1,13 @@
 using ModernGL, GLAbstraction, GLWindow, GLFW, Reactive, ImmutableArrays, Images, GLText, Quaternions, Color
 using GLPlot
-
-window  = createdisplay(w=1920, h=1080)
+windowhints =[
+  (GLFW.SAMPLES, 0), 
+  (GLFW.DEPTH_BITS, 0), 
+  (GLFW.ALPHA_BITS, 0), 
+  (GLFW.STENCIL_BITS, 0),
+  (GLFW.AUX_BUFFERS, 0)
+]
+window  = createdisplay(w=1920, h=1080, windowhints=windowhints, debugging=false)
 cam     = PerspectiveCamera(window.inputs, Vec3(1,0,0), Vec3(0))
 ocam    = OrthographicCamera(window.inputs[:window_size], Input(1f0), Input(Vec2(0)), Input(Vector2(1.0)))
 ocam2   = OrthographicPixelCamera(window.inputs)
@@ -22,56 +28,28 @@ parameters = [
         (GL_TEXTURE_MIN_FILTER, GL_NEAREST),
         (GL_TEXTURE_MAG_FILTER, GL_NEAREST)
     ]
-color     = Texture(GLfloat, 4, window.inputs[:window_size].value[3:4], format=GL_RGBA, internalformat=GL_RGBA8)
-stencil   = Texture(GLushort, 2, div(window.inputs[:window_size].value[3:4], 4), format=GL_RG_INTEGER, internalformat=GL_RG16UI, parameters=parameters)
+
+println(window.inputs[:framebuffer_size].value)
+framebuffsize = window.inputs[:framebuffer_size].value
+color     = Texture(GLfloat, 4, framebuffsize, format=GL_RGBA, internalformat=GL_RGBA8)
+stencil   = Texture(GLushort, 2, framebuffsize, format=GL_RG_INTEGER, internalformat=GL_RG16UI, parameters=parameters)
 glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, color.id, 0)
 glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, stencil.id, 0)
 
 rboDepthStencil = GLuint[0]
-OrthographicPixelCamera
+
 glGenRenderbuffers(1, rboDepthStencil);
 glBindRenderbuffer(GL_RENDERBUFFER, rboDepthStencil[1])
-glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, window.inputs[:window_size].value[3:4]...)
+glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, framebuffsize...)
 glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepthStencil[1])
 
 group_amount = Input(int32(2))
 
-analyzefb = glGenFramebuffers()
-glBindFramebuffer(GL_FRAMEBUFFER, analyzefb)
-analyzetex = Texture(GLushort, 2, [group_amount.value+1, 1], format=GL_RG_INTEGER, internalformat=GL_RG16UI, parameters=parameters)
-glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, analyzetex.id, 0)
-
 
 mousepos = lift(x-> Vec2(x...), window.inputs[:mouseposition])
 
-data = [
-  :dummy            => GLBuffer(GLfloat[0],1),
-  :index            => indexbuffer(GLuint[0]),
-  :stencil          => stencil,
-  :groups           => group_amount,
-  :mouseposition    => mousepos
-]
-glsl_view = [
-  "GLSL_EXTENSIONS"     => "#extension GL_ARB_draw_instanced : enable"
-]
-
-analyzeshader = TemplateProgram(
-  joinpath(shaderdir, "stencil_analyze.vert"), joinpath(shaderdir, "stencil_analyze.frag"), 
-  view=glsl_view,fragdatalocation=[(0, "fragment_color")]
-)
-analyzeRO = instancedobject(data, analyzeshader, prod(window.inputs[:window_size].value[3:4]), GL_POINTS)
-
-
-const framebufferdata = lift(group_amount) do x
-  [Vector2{GLushort}(zero(GLushort), zero(GLushort)) for i=1:x+1, j=1:1]
-end
-
-selectiondata = lift(window.inputs[:mouseposition], framebufferdata) do x, data
-    glBindTexture(GL_TEXTURE_2D, analyzetex.id)
-    glGetTexImage(GL_TEXTURE_2D, 0, analyzetex.format, analyzetex.pixeltype, data)
-    data
-end
-
+selectiondata = Input(Vector2{GLushort}[Vector2{GLushort}(zero(GLushort), zero(GLushort))])
+#lift(println, selectiondata)
 clickedselection = foldl((IntSet(),  Vector2(-1,-1)), selectiondata, window.inputs[:mousebuttonspressed]) do v0, selection, mousebuttonset
   if !isempty(mousebuttonset) # mousebutton 0==left clicked + and over text
     (mousebuttonset, Vector2(int(selection[1])...))
@@ -79,7 +57,6 @@ clickedselection = foldl((IntSet(),  Vector2(-1,-1)), selectiondata, window.inpu
     v0
   end
 end
-
 
 function GLPlot.toopengl(dict::Dict{Symbol, Any})
   result = RenderObject[]
@@ -108,52 +85,57 @@ obj2    = toopengl(color, camera=ocam)
 
 
 
-
+glClearColor(1,1,1,1)
+const mousehover = Array(Vector2{GLushort}, 1)
 function renderloop()
-
+  window_size = window.inputs[:framebuffer_size].value
+  glViewport(0,0, window_size...)
   glBindFramebuffer(GL_FRAMEBUFFER, fb)
   glDrawBuffers(2, [GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1])
-  glClearColor(0,0,0,0)
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
   render(obj)
 
-  glBindFramebuffer(GL_FRAMEBUFFER, analyzefb)
-  glDrawBuffer(GL_COLOR_ATTACHMENT0)
-  glDisable(GL_DEPTH_TEST)
-  glDisable(GL_CULL_FACE)
+  mousex, mousey = int([window.inputs[:mouseposition].value])
+  glReadBuffer(GL_COLOR_ATTACHMENT1)
+  glReadPixels(mousex, mousey, 1,1, stencil.format, stencil.pixeltype, mousehover)
+  @async push!(selectiondata, mousehover)
+
+  glReadBuffer(GL_COLOR_ATTACHMENT0)
+  glBindFramebuffer(GL_READ_FRAMEBUFFER, fb)
+  glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0)
   glClear(GL_COLOR_BUFFER_BIT)
-  glViewport(0, 0, group_amount.value, 1)
-  render(analyzeRO)
-
-  glBindFramebuffer(GL_FRAMEBUFFER, 0)
-  glViewport(0,0, window.inputs[:window_size].value[3:4]...)
-
-  glClearColor(1,1,1,1)
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-  render(obj2)
-
+  glBlitFramebuffer(0,0, window_size..., 0, 0, window_size..., GL_COLOR_BUFFER_BIT, GL_NEAREST)
 end
 
-
-glClearColor(0,0,0,1)
 
 const query = GLuint[1]
 const elapsed_time = GLuint64[1]
 const done = GLint[0]
 
+macro gputime(codeblock)
+  quote 
+    local const query = GLuint[1]
+    local const elapsed_time = GLuint64[1]
+    local const done = GLint[0]
+    glGenQueries(1, query)
+    glBeginQuery(GL_TIME_ELAPSED, query[1])
+    value = $(esc(codeblock))
+    glEndQuery(GL_TIME_ELAPSED)
+
+    while (done[1] != 1)
+      glGetQueryObjectiv(query[1],
+              GL_QUERY_RESULT_AVAILABLE,
+              done)
+    end 
+    glGetQueryObjectui64v(query[1], GL_QUERY_RESULT, elapsed_time)
+    println("Time Elapsed: ", elapsed_time[1] / 1000000.0, "ms")
+  end
+end
 while !GLFW.WindowShouldClose(window.glfwWindow)
   yield() # this is needed for react to work
-  glGenQueries(1, query)
-  glBeginQuery(GL_TIME_ELAPSED, query[1])
-    renderloop()
-  glEndQuery(GL_TIME_ELAPSED)
-  while (done[1] != 1)
-    glGetQueryObjectiv(query[1],
-            GL_QUERY_RESULT_AVAILABLE,
-            done)
-  end 
-  glGetQueryObjectui64v(query[1], GL_QUERY_RESULT, elapsed_time)
-  println("Time Elapsed: ", elapsed_time[1] / 1000000.0, "ms")
+
+  @gputime renderloop()
+
   GLFW.SwapBuffers(window.glfwWindow)
   GLFW.PollEvents()
 end
