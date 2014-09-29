94a5b32edf6c8a17f3b19b68a5e2c557f3a6b005
Simon Danisch <sdanisch@gmail.com>
2014-09-29 15:39:05 +0200

    added more test cases

--- test/images.jl
+++ test/images.jl
@@ -1,13 +1,28 @@
 using Base.Test
-using GLPlot, GLAbstraction, ModernGL, GLFW, Color, Images, Reactive
+using GLPlot, GLAbstraction, ModernGL, GLFW, Color, Images, Reactive, ImmutableArrays, FixedPointNumbers
 
 N = 54
 
 
-window = createdisplay(w=iround(N*9.1), h=N)
+window = createdisplay(w=iround(N*11.1), h=N)
 camera = OrthographicPixelCamera(window.inputs)
 
+
+immutable GLGlyph{T <: Real} <: AbstractVector{T}
+  glyph::T
+end
+Base.getindex(collection::GLGlyph, I::Integer) = I == 1 ? collection.glyph : error("Out of bounds")
+Base.length{T}(::GLGlyph{T}) = 1
+Base.length{T}(::Type{GLGlyph{T}}) = 1
+Base.eltype{T}(::GLGlyph{T}) = T
+Base.eltype{T}(::Type{GLGlyph{T}}) = T
+Base.convert{T}(::Type{GLGlyph{T}}, x::Char) = (int(x) >= 0 && int(x) <= 256) ? GLGlyph(convert(T, x)) : error("This char: ", x, " can't be converted to GLGlyph")
+
+
+
 test_images = {
+	[GLGlyph{Uint8}(33) for i=1:N, j=1:N], 
+	[Vector3{Ufixed8}(0, 1, 0) for i=1:N, j=1:N], 
 	[Vec4(0,1,0,1) for i=1:N, j=1:N], 
 	[Vec3(0,1,0)   for i=1:N, j=1:N], 
 	[AlphaColorValue(RGB(0f0,1f0,0f0), 1f0) for i=1:N, j=1:N],
@@ -33,3 +48,5 @@ end
 map(glplot, test_renderobjects)
 
 renderloop(window)
+
+
