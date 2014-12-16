9f13ebd1ce7b8c91eb34ff4a5b585416cdcbc516
Simon Danisch <sdanisch@gmail.com>
2014-12-16 17:48:35 +0100

    might fix Color import issue #20

--- example/mesh.jl
+++ example/mesh.jl
@@ -5,7 +5,7 @@ window = createdisplay(w=1920, h=1280)
 
 
 
-N = 100
+N = 50
 const vol = Float32[sin(x/15f0)+sin(y/15f0)+sin(z/15f0) for x=1:N, y=1:N, z=1:N]
 const msh = Meshes.isosurface(vol, 0.5f0, 0.001f0)
 
--- src/mesh.jl
+++ src/mesh.jl
@@ -142,9 +142,8 @@ tohsva(rgba)     = AlphaColorValue(convert(HSV, rgba.c), rgba.alpha)
 torgba(hsva)     = AlphaColorValue(convert(RGB, hsva.c), hsva.alpha)
 tohsva(h,s,v,a)  = AlphaColorValue(HSV(float32(h), float32(s), float32(v)), float32(a))
 Base.convert{T <: AbstractAlphaColorValue}(typ::Type{T}, x::AbstractAlphaColorValue) = AlphaColorValue(convert(RGB{eltype(typ)}, x.c), convert(eltype(typ), x.alpha))
-    
 
-RGB{T}(x::T) = RGB{T}(x,x,x)
+Color.RGB{T}(x::T) = RGB{T}(x,x,x)
 
 
 
