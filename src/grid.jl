481c1b73abd55f039eb23cc3dbac9501f481f8c2
Simon Danisch <sdanisch@gmail.com>
2015-01-15 20:25:33 +0100

    updated mistake

--- src/grid.jl
+++ src/grid.jl
@@ -1,11 +1,11 @@
 function creategrid(; bg_color=Vec4(1, 1, 1, 0.01), grid_color=Vec4(0,0,0,0.2),
-		xrange::(Real, Real)=(-1,1), yrange::(Real, Real)=(-1,1), zrange::(Real, Real)=(-1,1), 
+		xrange::(Real, Real)=(-1,1), yrange::(Real, Real)=xrange, zrange::(Real, Real)=xrange, 
 		grid_thickness::Vec3 = Vec3(2), gridsteps::Vec3 = Vec3(10), 
 		camera=pcamera
 	)
-	xyplane = genquad(Vec3(xrange[1], yrange[1], zrange[1]), Vec3(xrange[1], yrange[2], zrange[1]), Vec3(xrange[2], yrange[1], zrange[1]))
-	zyplane = genquad(Vec3(xrange[1], yrange[1], zrange[1]), Vec3(xrange[1], yrange[2], zrange[1]), Vec3(xrange[1], yrange[1], zrange[2]))
-	zxplane = genquad(Vec3(xrange[1], yrange[1], zrange[1]), Vec3(xrange[2], yrange[1], zrange[1]), Vec3(xrange[1], yrange[1], zrange[2]))
+	xyplane = genquad(Vec3(xrange[1], yrange[1], zrange[1]), Vec3(0, yrange[2], 0), Vec3(xrange[2], 0, 0))
+	zyplane = genquad(Vec3(xrange[1], yrange[1], zrange[1]), Vec3(0, yrange[2], 0), Vec3(0, 0, zrange[2]))
+	zxplane = genquad(Vec3(xrange[1], yrange[1], zrange[1]), Vec3(xrange[2], 0, 0), Vec3(0, 0, zrange[2]))
 	
 	v,uv,n,i = mergemesh(xyplane, zyplane, zxplane)
 
@@ -15,7 +15,7 @@ function creategrid(; bg_color=Vec4(1, 1, 1, 0.01), grid_color=Vec4(0,0,0,0.2),
 			:grid_color 		  	=> grid_color,
 			:bg_color 			  	=> bg_color,
 			:grid_thickness  		=> grid_thickness,
-			:gridsteps  		  	=> float32(gridsteps),
+			:gridsteps  		  	=> gridsteps,
 			:mvp 				    => camera.projectionview
 		)), gridshader)
 	prerender!(grid, glDisable, GL_DEPTH_TEST, glDisable, GL_CULL_FACE, enabletransparency)
