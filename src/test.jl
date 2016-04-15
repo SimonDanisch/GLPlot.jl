b015244c8f3bae6070a2927b51235fda0971bf5f
SimonDanisch <sdanisch@gmail.com>
2016-04-15 19:53:53 +0200

    add toggle screen

--- src/test.jl
+++ src/test.jl
@@ -4,9 +4,37 @@ w = glscreen();@async renderloop(w)
 tarea = map(w.area) do pa
     SimpleRectangle(0, 0, 120, pa.h)
 end
+
+function toggle(robj::Context, window)
+    foldp(false, window.inputs[:mouse_buttons_pressed]) do v0, mbp
+        if GLAbstraction.singlepressed(mbp, GLFW.MOUSE_BUTTON_LEFT)
+            id, index = value(mouse2id(window))
+            if id==robj.children[].id
+                return !v0
+            end
+        end
+        v0
+    end
+end
+edit_screen_show_button = layout(w.area,
+    visualize(
+        Rectangle(1-0.01,0.5,0.01, 0.01), color=RGBA{Float32}(0,0,0,1)
+    )
+)
+edit_screen_show_button = edit_screen_show_button[1]
+show_edit_screen = toggle(edit_screen_show_button, w)
+view(edit_screen_show_button, camera=:fixed_pixel)
+edit_screen_area = map(show_edit_screen, w.area) do visible, area
+    w = visible ? 160 : 0
+    SimpleRectangle(area.w-w, 0, w, area.h)
+end
 toolbar_screen = Screen(w, area=tarea)
+
+edit_screen_area
+
+edit_screen = Screen(w, area=edit_screen_area, color=RGBA{Float32}(0,0,0,1))
+
 tools = 5
-lerp(a, b, t) = a + t*(b-a)
 function update_positions(v0, mp, area)
     positions, colors = v0
     if isinside(area, mp...)
@@ -14,12 +42,13 @@ function update_positions(v0, mp, area)
             x = 1-(clamp(abs(p[2]+37 - mp[2]), 0, 200) / 200)
             y = clamp(abs(area.w-mp[1]), 0, 62)*x
             positions[i] = Point2f0(-60+y, p[2])
-            colors[i] = RGBA{Float32}(0.9,0.1,0.9,1)
+            alpha = 1-(clamp(abs(p[2]+37 - mp[2]), 0, 100) / 100)
+            colors[i] = RGBA{Float32}(0.8,0.8,0.8,alpha)
         end
     else
         for (i, p) in enumerate(linspace(0, area.h, length(positions)))
             positions[i] = Point2f0(-68, p)
-            colors[i] = RGBA{Float32}(0.9,0.9,0.9,0)
+            colors[i] = RGBA{Float32}(0.8,0.8,0.8,0)
         end
     end
     positions, colors
@@ -49,5 +78,5 @@ end
 tool_clicked = map(on_click, key_pressed)
 
 
-
 view(robj, toolbar_screen, camera=:fixed_pixel)
+view(visualize(rand(Float32, 32,32)))
