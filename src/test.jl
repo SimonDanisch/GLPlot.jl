da4fc3bc316413ccb63429cf60090c331be0c09e
SimonDanisch <sdanisch@gmail.com>
2016-04-21 09:04:25 +0200

    added icons

--- src/test.jl
+++ src/test.jl
@@ -1,10 +1,6 @@
-using Reactive, GLVisualize, GeometryTypes, Colors, GLWindow, GLAbstraction
-w = glscreen()
-
-tarea = map(w.area) do pa
-    SimpleRectangle(0, 0, 120, pa.h)
-end
-
+using Reactive, GLVisualize, GeometryTypes, Colors, GLWindow, GLAbstraction, GLFW, FileIO
+w = glscreen();@async renderloop(w)
+toolbar_area(pa) = SimpleRectangle(0, 0, 120, pa.h)
 function toggle(robj::Context, window)
     foldp(false, window.inputs[:mouse_buttons_pressed]) do v0, mbp
         if GLAbstraction.singlepressed(mbp, GLFW.MOUSE_BUTTON_LEFT)
@@ -16,25 +12,13 @@ function toggle(robj::Context, window)
         v0
     end
 end
-edit_screen_show_button = layout(w.area,
-    visualize(
-        Rectangle(1-0.01,0.5,0.01, 0.01), color=RGBA{Float32}(0,0,0,1)
-    )
-)
-edit_screen_show_button = edit_screen_show_button[1]
-show_edit_screen = toggle(edit_screen_show_button, w)
-view(edit_screen_show_button, camera=:fixed_pixel)
-edit_screen_area = map(show_edit_screen, w.area) do visible, area
-    w = visible ? 160 : 0
-    SimpleRectangle(area.w-w, 0, w, area.h)
+function edit_rectangle(visible, area, arrow_pos_s)
+    w = visible ? div(area.w,4) : 0
+    x = area.w-w
+    push!(arrow_pos_s, [Point2f0(x, area.h/2)])
+    SimpleRectangle(x, 0, w, area.h)
 end
-toolbar_screen = Screen(w, area=tarea)
-
-edit_screen_area
-
-edit_screen = Screen(w, area=edit_screen_area, color=RGBA{Float32}(0,0,0,1))
-
-tools = 5
+viewing_area(area) = SimpleRectangle(0, 0, area.x, area.h)
 function update_positions(v0, mp, area)
     positions, colors = v0
     if isinside(area, mp...)
@@ -46,77 +30,78 @@ function update_positions(v0, mp, area)
             colors[i] = RGBA{Float32}(0.8,0.8,0.8,alpha)
         end
     else
-        for (i, p) in enumerate(linspace(0, area.h, length(positions)))
+        for (i, p) in enumerate(linspace(74, area.h-74*2, length(positions)))
             positions[i] = Point2f0(-68, p)
             colors[i] = RGBA{Float32}(0.8,0.8,0.8,0)
         end
     end
     positions, colors
 end
-ma_posis = zeros(Point2f0, tools)
-ma_colsis = zeros(RGBA{Float32}, tools)
-
-ps = foldp(update_positions,
-    (ma_posis, ma_colsis),
-    w.inputs[:mouseposition], tarea
-)
-using GLFW
-key_pressed = const_lift(GLAbstraction.singlepressed, w.inputs[:mouse_buttons_pressed], GLFW.MOUSE_BUTTON_LEFT)
-const m2id = mouse2id(w)
-scale = Signal(Vec2f0(74))
-robj = visualize((Circle, map(first, ps)), scale=scale, glow_width=2f0, glow_color=map(last, ps))
-
-function on_click(isclicked)
+function on_click(isclicked, m2id, scalzies, scale, scaleclick, tools)
     id, index = value(m2id)
     if isclicked && id==robj.children[].id && tools >= index
-        push!(scale, Vec2f0(76))
+        s = value(scalzies)
+        s[index] = Vec2f0(scaleclick)
+        push!(scalzies, s)
         return index
     end
-    push!(scale, Vec2f0(74))
+    push!(scalzies, fill(Vec2f0(scale), tools))
     return 0
 end
-tool_clicked = map(on_click, key_pressed)
 
+glload(name) = load(Pkg.dir("GLPlot", "src", "icons", name))
+images = Matrix{BGRA{U8}}[glload("cube_camera.png").data, glload("screenshot.png").data, glload("wasd_camera.png").data]
 
-view(robj, toolbar_screen, camera=:fixed_pixel)
-view(visualize(rand(Float32, 32,32)))
-@async renderloop(w)
-
-
-
+tools = length(images)
 tarea = map(toolbar_area, w.area)
-button_pos = Signal(Point2f0(w.area.value.w, w.area.value.h/2))
+button_pos = Signal([Point2f0(w.area.value.w, w.area.value.h/2)])
 edit_screen_show_button = visualize(
-    (Rectangle(10,10,10,20), button_pos), 
+    (SimpleRectangle(-10,-10,10,20), button_pos), 
     color=RGBA{Float32}(0.6,0.6,0.6,1)
 )
 
 show_edit_screen = toggle(edit_screen_show_button, w)
-view(edit_screen_show_button, camera=:fixed_pixel)
 edit_screen_area = map(edit_rectangle, 
     show_edit_screen, w.area, Signal(button_pos)
 )
 
+viewing_screen = Screen(w, 
+    area=map(viewing_area, edit_screen_area), 
+    color=RGBA{Float32}(1,1,1,0)
+)
 toolbar_screen = Screen(w, area=tarea)
+edit_screen    = Screen(w, area=edit_screen_area, color=RGBA{Float32}(0,0,0,1))
 
-edit_screen = Screen(w, area=edit_screen_area, color=RGBA{Float32}(0,0,0,1))
-
-tools = 5
 
 ma_posis = zeros(Point2f0, tools)
 ma_colsis = zeros(RGBA{Float32}, tools)
+scalzies = Signal(fill(Vec2f0(74), tools))
 
 ps = foldp(update_positions,
     (ma_posis, ma_colsis),
     w.inputs[:mouseposition], tarea
 )
 
-key_pressed = const_lift(GLAbstraction.singlepressed, w.inputs[:mouse_buttons_pressed], GLFW.MOUSE_BUTTON_LEFT)
+key_pressed = const_lift(GLAbstraction.singlepressed, 
+    w.inputs[:mouse_buttons_pressed], 
+    GLFW.MOUSE_BUTTON_LEFT
+)
 const m2id = mouse2id(w)
-scale = Signal(Vec2f0(74))
-robj = visualize((Circle, map(first, ps)), scale=scale, glow_width=2f0, glow_color=map(last, ps))
+
+robj = visualize(
+    (Circle, map(first, ps)),
+    image=Texture(images),
+    scale=scalzies, glow_width=2f0, 
+    glow_color=map(last, ps)
+)
 
 
-tool_clicked = map(on_click, key_pressed)
+tool_clicked = map(
+    on_click, 
+    key_pressed, Signal(m2id), Signal(scalzies),
+    Signal(74), Signal(76), Signal(tools)
+)
+
 view(robj, toolbar_screen, camera=:fixed_pixel)
-view(visualize(rand(Float32, 32,32)))
+view(edit_screen_show_button, camera=:fixed_pixel)
+view(visualize(rand(Float32, 32,32)), viewing_screen)
