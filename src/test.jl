27c31e00ce0175a1f7878b1b6427bf4b4b69bdcc
SimonDanisch <sdanisch@gmail.com>
2016-04-25 11:13:53 +0200

    better toolbar

--- src/test.jl
+++ src/test.jl
@@ -1,5 +1,6 @@
 using Reactive, GLVisualize, GeometryTypes, Colors, GLWindow, GLAbstraction, GLFW, FileIO
-w = glscreen();@async renderloop(w)
+w = glscreen()
+icon_size = 74
 toolbar_area(pa) = SimpleRectangle(0, 0, 120, pa.h)
 function toggle(robj::Context, window)
     foldp(false, window.inputs[:mouse_buttons_pressed]) do v0, mbp
@@ -56,22 +57,24 @@ tools = length(images)
 tarea = map(toolbar_area, w.area)
 button_pos = Signal([Point2f0(w.area.value.w, w.area.value.h/2)])
 edit_screen_show_button = visualize(
-    (SimpleRectangle(-10,-10,10,20), button_pos), 
+    (SimpleRectangle(-10,-10,10,20), button_pos),
     color=RGBA{Float32}(0.6,0.6,0.6,1)
 )
 
 show_edit_screen = toggle(edit_screen_show_button, w)
-edit_screen_area = map(edit_rectangle, 
+edit_screen_area = map(edit_rectangle,
     show_edit_screen, w.area, Signal(button_pos)
 )
 
-viewing_screen = Screen(w, 
-    area=map(viewing_area, edit_screen_area), 
+viewing_screen = Screen(w,
+    area=map(viewing_area, edit_screen_area),
     color=RGBA{Float32}(1,1,1,0)
 )
+
 toolbar_screen = Screen(w, area=tarea)
 edit_screen    = Screen(w, area=edit_screen_area, color=RGBA{Float32}(0,0,0,1))
-
+cam, button_pers, cube = cubecamera(viewing_screen)
+layout!(SimpleRectangle(0,0,icon_size, icon_size), cube)
 
 ma_posis = zeros(Point2f0, tools)
 ma_colsis = zeros(RGBA{Float32}, tools)
@@ -82,8 +85,8 @@ ps = foldp(update_positions,
     w.inputs[:mouseposition], tarea
 )
 
-key_pressed = const_lift(GLAbstraction.singlepressed, 
-    w.inputs[:mouse_buttons_pressed], 
+key_pressed = const_lift(GLAbstraction.singlepressed,
+    w.inputs[:mouse_buttons_pressed],
     GLFW.MOUSE_BUTTON_LEFT
 )
 const m2id = mouse2id(w)
@@ -91,17 +94,20 @@ const m2id = mouse2id(w)
 robj = visualize(
     (Circle, map(first, ps)),
     image=Texture(images),
-    scale=scalzies, glow_width=2f0, 
+    scale=scalzies, glow_width=2f0,
     glow_color=map(last, ps)
 )
 
 
 tool_clicked = map(
-    on_click, 
+    on_click,
     key_pressed, Signal(m2id), Signal(scalzies),
-    Signal(74), Signal(76), Signal(tools)
+    Signal(icon_size), Signal(icon_size+2), Signal(tools)
 )
 
 view(robj, toolbar_screen, camera=:fixed_pixel)
+view(cube, toolbar_screen, camera=:fixed_pixel)
 view(edit_screen_show_button, camera=:fixed_pixel)
 view(visualize(rand(Float32, 32,32)), viewing_screen)
+
+renderloop(w)
