7e2ebf022c59c200ecd1cc963f7e7427076da26f
SimonDanisch <sdanisch@gmail.com>
2017-03-15 12:37:32 +0100

    update example

--- example/volume.jl
+++ example/volume.jl
@@ -1,5 +1,6 @@
 using GLPlot, GLVisualize, GLAbstraction, Colors, GeometryTypes, FileIO
 using Reactive, GLWindow, NIfTI
+const If0 = GLVisualize.Intensity{1, Float32}
 window = GLPlot.init()
 
 layout = [
@@ -8,9 +9,14 @@ layout = [
 ]
 
 screens = GLVisualize.layoutscreens(window, layout)
-vol = niread(joinpath(homedir(), "Desktop", "brain.nii")).raw;
-vol = vol ./ maximum(vol)
-const If0 = GLVisualize.Intensity{1, Float32}
+function volume_data(N)
+    vol = Float32[sin(x/15.0) + sin(y/15.0) + sin(z/15.0) for x=1:N, y=1:N, z=1:N]
+    max = maximum(vol); min     = minimum(vol)
+    vol = (vol .- min) ./ (max .- min)
+end
+
+vol = volume_data(128)
+
 
 # to change color_map use the interactive edit menu or pass an array of color
 # via color_map = Vector{Colorant}
@@ -19,10 +25,14 @@ slice_screens = vcat(screens[2][2], screens[1]...)
 for i = 1:3
     range_s = play_widget(1:size(vol, i))
     slice = map(range_s) do slice_idx
-        idx = ntuple(d-> d == i ? slice_idx : (:), 3)
+        idx = ntuple(d-> d == i ? slice_idx : (:), Val{3})
         # This conversion is necessary but will be automatic soon!
-        permutedims(If0.(view(vol, idx...)), (2, 1))
+        If0.(view(vol, idx...))
     end
-    plot = glplot(slice, screen = slice_screens[i], camera = :orthographic_pixel)
-    center!(slice_screens[i],:orthographic_pixel)
+    plot = glplot(
+        slice, screen = slice_screens[i],
+        color_norm = Vec2f0(0, 1),
+        camera = :orthographic_pixel
+    )
+    center!(slice_screens[i], :orthographic_pixel)
 end
